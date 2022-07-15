5b591b7507634f81476ddfec4c06b994ae12d615
HiteshGehu <91263054+HiteshGehu@users.noreply.github.com>
2022-07-16 01:42:41 +0530

    Add files via upload

---
+++ codes/footer.php
@@ -0,0 +1,29 @@
+<?php ?>
+    <footer>
+      <section class="ft-social">
+        <ul class="ft-social-list">
+          <li>
+            <a href="#"><i class="bx bxl-meta"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="bx bxl-instagram"></i></a>
+          </li>
+          <li>
+            <a href="mailto: hiteshkandpal29032003@gmail.com" target="_blank"
+              ><i class="bx bxl-gmail"></i
+            ></a>
+          </li>
+          <li>
+            <a href="#"><i class="bx bxl-linkedin"></i></a>
+          </li>
+        </ul>
+      </section>
+      
+      <section class="ft-legal">
+        <ul class="ft-legal-list">
+          <li ><a href="#">Phone NO. - 0011 2525 4949</a></li>
+          <li ><a href="#">Email - hiteshkandpal29032003@gamil.com</a></li>
+          <li>&copy; Graphic Era Hill</li>
+        </ul>
+      </section>
+    </footer>
